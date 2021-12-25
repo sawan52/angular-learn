52f14f46cf104bf73da55e85fc9976f7838e7ed5
Sawan Singh <sawansingh5216977@gmail.com>
2021-12-25 16:56:28 +0530

    print input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,30 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '                                 ';
   versionNumber = 12;
+  displayVal = '';
 
-  getData(data:string){
+  getData(data: any) {
     console.log(data);
+    this.displayVal = data;
   }
 
-  getName(name: string){
-    alert("My name is " + name);
+  getName(name: string) {
+    alert('My name is ' + name);
   }
 
-  compareValue(){
-    if(this.title.trim() == "blog"){
-      console.log("title length: " + this.title.length);
-      console.log("trim title length: " + this.title.trim().length);
-      alert("Trim worked!");
-    }else{
-      console.log("title length: " + this.title.length);
-      console.log("trim title length: " + this.title.trim().length);
-      alert("Trim did not worked!");
+  compareValue() {
+    if (this.title.trim() == 'blog') {
+      console.log('title length: ' + this.title.length);
+      console.log('trim title length: ' + this.title.trim().length);
+      alert('Trim worked!');
+    } else {
+      console.log('title length: ' + this.title.length);
+      console.log('trim title length: ' + this.title.trim().length);
+      alert('Trim did not worked!');
     }
   }
 }
-
