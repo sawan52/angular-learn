f35eb888377e8d06088555a80dfb86594cbab919
Sawan Singh <sawansingh5216977@gmail.com>
2021-12-27 13:58:19 +0530

    dynamic style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,45 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   versionNumber = 12;
+  color = "blue";
+  bgColor = "green";
 
-  users = ['Sawan', 'Yash', 'Akash', 'Rohit', 'Ravi', 'Satyam'];
-
-  user_detail = [
-    {
-      name: 'Sawan',
-      email: 'Sawan@gmail.com',
-      phone: '8956231400',
-      socialAccounts: ['facebook', 'yahoo', 'gmail'],
-    },
-    {
-      name: 'Yash',
-      email: 'Yash@gmail.com',
-      phone: '7854120325',
-      socialAccounts: ['yahoo', 'facebook', 'gmail'],
-    },
-    {
-      name: 'Akash',
-      email: 'Akash@gmail.com',
-      phone: '1204589600',
-      socialAccounts: ['gmail', 'yahoo', 'facebook'],
-    },
-    {
-      name: 'Rohit',
-      email: 'Rohit@gmail.com',
-      phone: '8562001122',
-      socialAccounts: ['facebook', 'gmail', 'yahoo'],
-    },
-    {
-      name: 'Ravi',
-      email: 'Ravi@gmail.com',
-      phone: '7788554422',
-      socialAccounts: ['facebook', 'yahoo', 'gmail'],
-    },
-    {
-      name: 'Satyam',
-      email: 'Satyam@gmail.com',
-      phone: '9966558844',
-      socialAccounts: ['yahoo', 'facebook', 'gmail'],
-    },
-  ];
+  updateColor(){
+    this.color = "red";
+    this.bgColor = "blue";
+  }
 }
